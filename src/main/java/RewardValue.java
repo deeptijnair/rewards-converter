96722e6adc4d756a9de32aa4a6542db6b10c60b1
Deepthi <deeptijnair@gmail.com>
2024-08-22 14:02:59 -0400

    Added and Implemented the RewardsValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+
+    double cashValue;
+    int miles;
+    RewardValue(double cashValue){
+        this.cashValue = cashValue;
+    }
+
+    RewardValue(int miles){
+        this.miles = miles;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+      //  double cash = getCashValue();
+        miles = (int) ((int)cashValue * 0.0035);
+        return miles;
+    }
 }
